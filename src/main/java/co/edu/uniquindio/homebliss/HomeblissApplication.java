ff5bb4f7b9083ba6888d1a30d544172fed2305b3
BrandonOrozcoZ <brandons.orozcoz@uqvirtual.edu.co>
2023-03-02 10:33:21 -0500

    Fisrt commit

---
+++ src/main/java/co/edu/uniquindio/homebliss/HomeblissApplication.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.homebliss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class HomeblissApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeblissApplication.class, args);
+    }
+}
