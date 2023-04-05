1c4030bc4efdebe6f2f717a64b118ce4669be7b9
BrandonOrozcoZ <brandons.orozcoz@uqvirtual.edu.co>
2023-04-04 19:41:40 -0500

    (Feature) Se agregaron validaciones para los campos de productDTO

--- src/main/java/co/edu/uniquindio/homebliss/dto/ProductDTO.java
+++ src/main/java/co/edu/uniquindio/homebliss/dto/ProductDTO.java
@@ -1,9 +1,15 @@
 package co.edu.uniquindio.homebliss.dto;
 
 import co.edu.uniquindio.homebliss.model.Category;
+import jakarta.persistence.Column;
+import jakarta.persistence.Lob;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.List;
 
@@ -12,17 +18,34 @@ import java.util.List;
 @Setter
 public class ProductDTO {
 
+    @NotNull(message = "El nombre no puede ser null")
+    @NotBlank(message = "El nombre no puede estar vacía")
+    @Length(max = 150, message = "El nombre debe ser de máximo 250 caracteres")
     private String name;
 
+    @NotNull(message = "El apellido no puede ser null")
+    @NotBlank(message = "El apellido no puede estar vacía")
+    @Lob
     private String description;
 
+    @NotNull(message = "Las unidades en stock no puede ser null")
+    @NotBlank(message = "Las unidades en stock no puede estar vacía")
+    @PositiveOrZero
     private int stock;
 
+    @NotNull(message = "El precio no puede ser null")
+    @NotBlank(message = "El precio no puede estar vacío")
+    @PositiveOrZero
     private float price;
 
+    @NotNull(message = "El código del vendedor no puede ser null")
+    @NotBlank(message = "El código del vendedor no puede estar vací0")
     private int sellerCode;
 
+    @NotNull(message = "Las imagenes no pueden ser null")
     private List<String> images;
 
+
+    @NotNull(message = "Las categorías no pueden ser null")
     private List<Category> categories;
 }
